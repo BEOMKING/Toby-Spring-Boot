e406c543d8e8d0a6cff0b2425573a2b9fdcbefa3
BEOMKING <qjawlsqjacks@naver.com>
2023-01-29 16:30:20 +0900

    @SpringBootApplication 내부 동작 방식 구현

--- src/main/java/helloboot/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/helloboot/tobyspringboot/TobySpringBootApplication.java
@@ -1,12 +1,10 @@
 package helloboot.tobyspringboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -23,26 +21,7 @@ public class TobySpringBootApplication {
     }
 
     public static void main(String[] args) {
-        // Spring Container
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                // Servlet Container (Web Application Server)
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext ->
-                        servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                                .addMapping("/*")
-                );
-                webServer.start();
-                super.onRefresh();
-            }
-        };
-
-        context.register(TobySpringBootApplication.class);
-        context.refresh();
+        MySpringBootApplication.run(TobySpringBootApplication.class, args);
     }
-    
+
 }
