d83c0904793ccc25d804de5ca39b050c90445e47
BEOMKING <qjawlsqjacks@naver.com>
2023-01-29 14:44:47 +0900

    Spring Container 생성 시점에 ServletContainer 생성 추가 & Fatory 패턴으로 Bean 등록

--- src/main/java/helloboot/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/helloboot/tobyspringboot/TobySpringBootApplication.java
@@ -3,46 +3,46 @@ package helloboot.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+@Configuration
 public class TobySpringBootApplication {
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new ComplexHelloService();
+    }
 
     public static void main(String[] args) {
-        GenericApplicationContext context = new GenericApplicationContext();
-        context.registerBean(HelloController.class);
-        context.registerBean(HelloService.class, ComplexHelloService::new);
-        context.registerBean(HelloService.class, SimpleHelloService::new);
+        // Spring Container
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                // Servlet Container (Web Application Server)
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+                WebServer webServer = serverFactory.getWebServer(servletContext ->
+                        servletContext.addServlet("dispatcherServlet",
+                                new DispatcherServlet(this)
+                        ).addMapping("/*")
+                );
+                webServer.start();
+//                getServletContext().setAttribute(
+//                    DispatcherServlet.WEB_APPLICATION_CONTEXT_ATTRIBUTE, this);
+                super.onRefresh();
+            }
+        };
+
+        context.register(TobySpringBootApplication.class);
         context.refresh();
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
-        WebServer webServer = serverFactory.getWebServer(servletContext ->
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) {
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = context.getBean(HelloController.class);
-                        String result = helloController.hello(name);
-
-                        resp.getWriter().write(result);
-                    } else {
-                        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                    }
-                }
-            }).addMapping("/hello")
-        );
-
-        webServer.start();
     }
     
 }
