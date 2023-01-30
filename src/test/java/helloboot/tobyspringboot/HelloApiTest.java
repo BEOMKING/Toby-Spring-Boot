937f59f12e25753306d81102f5a8919314ce271a
BEOMKING <qjawlsqjacks@naver.com>
2023-01-30 12:56:57 +0900

    API 테스트 코드

---
+++ src/test/java/helloboot/tobyspringboot/HelloApiTest.java
@@ -0,0 +1,23 @@
+package helloboot.tobyspringboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloApiTest {
+    @Test
+    void helloApi() {
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+        ResponseEntity<String> result = testRestTemplate.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        assertAll(() -> {
+            assertEquals("Complex Hello, Spring", result.getBody());
+            assertEquals(200, result.getStatusCodeValue());
+        });
+    }
+}
