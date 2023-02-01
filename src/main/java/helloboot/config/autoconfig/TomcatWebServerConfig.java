aa0a0df563e3bc8fed73992bb107c7dacc28d99c
BEOMKING <qjawlsqjacks@naver.com>
2023-02-01 21:58:03 +0900

    Configuration 동작 원리

--- src/main/java/helloboot/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/helloboot/config/autoconfig/TomcatWebServerConfig.java
@@ -1,11 +1,12 @@
 package helloboot.config.autoconfig;
 
+import helloboot.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
