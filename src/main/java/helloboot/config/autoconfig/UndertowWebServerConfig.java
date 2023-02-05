5864df295c45c1c5156f3387b2a019c8275551fa
BEOMKING <qjawlsqjacks@naver.com>
2023-02-06 00:43:12 +0900

    WAS Custom Bean

--- src/main/java/helloboot/config/autoconfig/UndertowWebServerConfig.java
+++ src/main/java/helloboot/config/autoconfig/UndertowWebServerConfig.java
@@ -2,6 +2,7 @@ package helloboot.config.autoconfig;
 
 import helloboot.config.ConditionalMyOnClass;
 import helloboot.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("io.undertow.Undertow")
 public class UndertowWebServerConfig {
     @Bean("undertowServletWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new UndertowServletWebServerFactory();
     }
